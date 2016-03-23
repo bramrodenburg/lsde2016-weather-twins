42ce4e1c86b67503a6d392d859f8c0d14ed62a0b
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 16:15:00 +0100

    Added 'sys' in front of argv

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -61,7 +61,7 @@ month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a,
 		x1min, x2min, x3min, x4min, x1max, x2max, x3max, x4max)))
 month_data.persist()
 #month_data = month_data.coalesce(1, True)
-if len(argv) > 1 and argv[1] == '*':
+if len(sys.argv) > 1 and sys.argv[1] == '*':
 	c = (2015-1901)*12
 else:
 	c = 12
