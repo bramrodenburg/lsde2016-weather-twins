0bc346c3a1e200f4ec41dae307f870962abbe5a0
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 10:49:55 +0100

    Fixed rounding bug.

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -29,7 +29,7 @@ month_data = month_data.combineByKey(lambda value: (value[0] if value[0] != None
 					utils.get_value(value[3])+x[6], 1 + x[7], (math.sin(value[4]*math.pi/180.0) if value[4]!=None else 0) + x[8], (math.cos(value[4]*math.pi/180.0) if value[4] != None else 0) + x[9]),\
 				lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4], x[5]+y[5], x[6]+y[6], x[7]+y[7], x[8]+y[8],\
 					x[9]+y[9])) 
-month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a, x5b)): (label, (x1/c1, x2/c2, x3/c3, x4/c4, math.atan2(x5a, x5b))))
+month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a, x5b)): (label, (float(x1)/c1, float(x2)/c2, float(x3)/c3, float(x4)/c4, math.atan2(x5a, x5b))))
 month_data = month_data.coalesce(1, True)
 month_data.saveAsTextFile("%s%s-%s" % (hdfs_results_path, start_time, 'all'))
 
