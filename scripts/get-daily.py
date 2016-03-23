f29cf82a7a735940727d44b5646efd9af2b28a94
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 10:35:43 +0100

    Fixed stuff

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -25,8 +25,8 @@ month_data = month_data.combineByKey(lambda value: (value[0] if value[0] != None
 					value[3] if value[3] != None else 0, 1 if value[3] != None else 0, \
 					math.sin(value[4]*math.pi/180.0) if value[4] != None else 0, \
 					math.cos(value[4]*math.pi/180.0) if value[4] != None else 0),\
-				lambda x, value: (utils.get_value(x[0]) + value[0], value[1] + 1, utils.get_value(x[1])+value[2], 1 + value[3], utils.get_value(x[3]) + value[4], 1 + value[5],\
-					utils.get_value(x[6])+value[6], 1 + value[7], utils.get_value(x[8]) + value[8], utils.get_value(x[9]) + value[9]),\
+				lambda x, value: (utils.get_value(x[0]) + value[0], value[1] + 1, utils.get_value(x[1])+value[2], 1 + value[3], utils.get_value(x[2]) + value[4], 1 + value[5],\
+					utils.get_value(x[3])+value[6], 1 + value[7], utils.get_value(x[4]) + value[8], utils.get_value(x[5]) + value[9]),\
 				lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4], x[5]+y[5], x[6]+y[6], x[7]+y[7], x[8]+y[8],\
 					x[9]+y[9])) 
 month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a, x5b)): (label, (x1/c1, x2/c2, x3/c3, x4/c4, math.atan2(x5a, x5b))))
