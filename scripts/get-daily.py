fe1016e13d9a57065b8bebfe2b6f837e62035629
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 11:50:08 +0100

    Corrected error in variance

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -37,10 +37,10 @@ month_data = month_data.combineByKey(lambda value: (value[0] if value[0] != None
 				lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4], x[5]+y[5], x[6]+y[6], x[7]+y[7], x[8]+y[8],\
 					x[9]+y[9], x[10]+y[10], x[11]+y[11], x[12]+y[12], x[13]+y[13])) 
 month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a, x5b, x1sq, x2sq, x3sq, x4sq)): (label, (float(x1)/c1 if c1>0 else "NaN", float(x2)/c2 if c2>0 else "NaN", float(x3)/c3 if c3>0 else "NaN", float(x4)/c4 if c4>0 else "NaN", math.atan2(x5a, x5b),\
-		1.0/(c1-1)*(x1sq+2*(float(x1)/c1)*x1+c1*(float(x1)/c1)**2) if c1>1 else "NaN", \
-		1.0/(c2-1)*(x2sq+2*(float(x2)/c2)*x2+c2*(float(x2)/c2)**2) if c2>1 else "NaN", \
-		1.0/(c3-1)*(x3sq+2*(float(x3)/c3)*x3+c3*(float(x3)/c3)**2) if c3>1 else "NaN", \
-		1.0/(c4-1)*(x4sq+2*(float(x4)/c4)*x4+c4*(float(x4)/c4)**2) if c4>1 else "NaN")))
+		1.0/(c1-1)*(x1sq-2*(float(x1)/c1)*x1+c1*(float(x1)/c1)**2) if c1>1 else "NaN", \
+		1.0/(c2-1)*(x2sq-2*(float(x2)/c2)*x2+c2*(float(x2)/c2)**2) if c2>1 else "NaN", \
+		1.0/(c3-1)*(x3sq-2*(float(x3)/c3)*x3+c3*(float(x3)/c3)**2) if c3>1 else "NaN", \
+		1.0/(c4-1)*(x4sq-2*(float(x4)/c4)*x4+c4*(float(x4)/c4)**2) if c4>1 else "NaN")))
 month_data = month_data.coalesce(1, True)
 month_data.saveAsTextFile("%s%s-%s" % (hdfs_results_path, start_time, 'all'))
 
