c66105a95699c6a28166b8ee29e193ec29a2eb14
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 16:27:55 +0100

    Added debug

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -66,6 +66,7 @@ if len(sys.argv) > 1 and sys.argv[1] == '*':
 else:
 	c = 12
 
+print month_data.collect()
 month_data = month_data.partitionBy(c, lambda x: x[0][0]*100 + x[0][1])
 month_data.saveAsTextFile("%s%s-%s" % (hdfs_results_path, start_time, 'all'))
 '''
