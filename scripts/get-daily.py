816b15b3e10b66cb6a41faf2ed3252d98f590faf
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-23 10:17:37 +0100

    Fixed bug.

--- scripts/get-daily.py
+++ scripts/get-daily.py
@@ -26,7 +26,7 @@ month_data = month_data.combineByKey(lambda value: (x[0] if x[0] != None else 0,
 					math.sin(x[4])*math.pi/180.0 if x[4] != None else 0, math.cos(x[4]).math.pi/180.0 if x[4] != None else 0, ),\
 				lambda x, value: (x[0] + value[0], value[1] + 1, x[1]+value[2], 1 + value[3], x[3] + value[4], 1 + value[5],\
 					x[6]+value[6], 1 + value[7], x[8] + value[8], x[9] + value[9]),\
-				lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4], x[5]+y[5], x[6]+y[6], x[7]+y[7], x[8]+y[8]\
+				lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4], x[5]+y[5], x[6]+y[6], x[7]+y[7], x[8]+y[8],\
 					x[9]+y[9])) 
 month_data = month_data.map(lambda (label, (x1, c1, x2, c2, x3, c3, x4, c4, x5a, x5b)): (label, (x1/c1, x2/c2, x3/c3, x4/c4, math.atan2(x5a, x5b))))
 month_data = month_data.coalesce(1, True)
